6766c4ba217c0a8785c4c0c1b9d6474d3b1884aa
Geoff Musick <musick.geoff@gmail.com>
2020-01-30 10:47:09 -0500

    Camera script rough draft

---
+++ Maze Game/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+
+
+    private void BasicFollowCamera (Vector3 tPos, Vector3 tUp, Vector3 tForward, float hDist, float vDist)
+    {
+        //Eye is offset from the target position
+        Vector3 eye = tPos - tForward * hDist + tUp * vDist;
+
+        //Camera forward is from eye to target
+        Vector3 cameraForward = tPos - eye;
+        cameraForward.Normalize();
+
+        
+
+    }
+
+}
--- Maze Game/Assets/Scripts/PlayerController.cs
+++ Maze Game/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 3;
     private Rigidbody rb;
 
     // Start is called before the first frame update
@@ -13,7 +14,7 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    /*void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
@@ -21,5 +22,12 @@ public class PlayerController : MonoBehaviour
         Vector3 input = new Vector3(horizontal, 0, vertical);
 
         rb.AddForce(input*5);
+    }*/
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        float vertical = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        transform.Translate(horizontal, 0, vertical);
     }
 }
